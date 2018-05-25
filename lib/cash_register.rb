7fe75741698c98d3a58c4330e0302ca3d787bde3
Laura Messa Travin <laura.travin@gmail.com>
2018-05-25 20:13:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,8 @@ def add_item(prod, price,q=1)
 
 end
 def apply_discount
-    self.total = self.total*( 1- b/100.to_f)
-  
+    self.total = self.total*( 1- self.discount/100.to_f)
+
 end
 
 def void_last_transaction
