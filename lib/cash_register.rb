d75a7cfbe0491bb29b748e60a8c06198f648cba0
Laura Messa Travin <laura.travin@gmail.com>
2018-05-25 19:53:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,4 +5,15 @@ attr_accessor :total, :discount
     @discount = d
  end
 
+ def discount
+   @discount = 20
+ end
+def add_item
+end
+def apply_discount
+end
+
+def void_last_transaction
+end
+
 end
